71b23c1fa19fab3a94e91150cf2ccbd51b9c094f
Padmajase <padmajapawar7@gmail.com>
2022-05-13 23:18:24 -0700

    Added : Ride class for cab invoice generator

---
+++ Ride.java
@@ -0,0 +1,11 @@
+package Ride;
+
+public class Ride {
+    public double distanceR;
+    public final int timeR;
+
+    public Ride(double distance, int time) {
+        this.distanceR = distance;
+        this.timeR = time;
+    }
+}
